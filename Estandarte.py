63d24b78c7510d0c317b2d64615bbcecf4e9096a
Ellan Jude <ellanjudeestandarte@gmail.com>
2024-09-02 19:53:42 +0800

    home, about me, autobiography added

--- Estandarte.py
+++ Estandarte.py
@@ -1,20 +1,70 @@
 import streamlit as st
 
-# Title of the app
-st.title("Basic Streamlit App")
+def show_home():
+    st.title("You have arrived")
+    
+    st.image("img/pic1.jpg", use_column_width=True)
+    
+    st.write("""
+    Foolishness, Dante. Foolishness. Might control everything. And without power you can not protect anything. Let alone yourself. 
+    Also, the autbiography and portfolio is in the navbar, top left.
+    """)
+    
+def show_about():
+    st.title("Greetings human!")
+    
+    with st.container():
+        col1, col2 = st.columns([1, 2])  
+        with col1:
+            st.image('img/profile.jpg')  
+        with col2:
+            st.title("I'm Ellan Jude Estandarte")
+            st.subheader("4th Year BSIT Student")
+            st.write('At Cebu Institute of Technology - University.')
 
-# Input text box for user's name
-name = st.text_input("Enter your name:")
+    st.write('---')
 
-# Input number box for user's age
-age = st.number_input("Enter your age:", min_value=0, max_value=120)
+    with st.container():
+        col3, col4 = st.columns(2)
+        with col3:
+            st.subheader("Education")
+            st.write("""
+            - Cebu Institute of Technology - University
+                - Bachelor of Science in Information Technology (Currently)
+            - Siaton Science High School
+                - Senior High - Science, Technology, Engineering and Mathematics (2020)
+            - Siaton National High School
+                - Junior High (2018)
+            - Felipe Tayko Memorial School
+                - Elementary (2012)       
+            """)
 
-# Button to trigger the action
-if st.button("Submit"):
-    if name and age:
-        st.write(f"Hello, {name}! You are {age} years old.")
-    else:
-        st.write("Please enter both your name and age.")
+def show_portfolio():
+    st.title("Portfolio")
+    
+    with st.container():
+        st.header("My Projects")
+        col1, col2 = st.columns((1, 2))
+        col3, col4 = st.columns((1, 2))
+        with col1:
+            st.image('img/front.jpg')
+        with col2:
+            st.subheader('CIMP-Frontend')
+            st.write('The frontend for our Capstone. It uses React and MUI. The project is about managing assets from our school property.')
+            st.markdown('[Github Repo](https://github.com/rosnuker/CIMP-Frontend)')
+        with col3:
+            st.image('img/back.jpg')
+        with col4:
+            st.subheader('CIMP-Backend')
+            st.write('The backend for our Capstone. It uses Springboot.')
+            st.markdown('[Github Repo](https://github.com/reeyyyxd/CIMP_Backend)')
+            
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("", ("Home", "About Me", "Portfolio"))
 
-# Footer
-st.write("Thank you for using the app SHEESH!")
+if page == "Home":
+    show_home()
+elif page == "About Me":
+    show_about()
+elif page == "Portfolio":
+    show_portfolio()
